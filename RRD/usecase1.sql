1c740e3b0fa00e38f0d40c4d8ac8d456c406f4ad
tihcar <rachit@inncretech.com>
2019-04-03 14:07:59 -0500

    Update usecase1.sql

--- RRD/usecase1.sql
+++ RRD/usecase1.sql
@@ -2,4 +2,11 @@ use warehouse saggezza_wh_2;
 use database demo_db;
 use schema public;
 
-select count(*) from test
+insert into test values('B');
+insert into test values('C');
+insert into test values('D');
+insert into test values('E');
+
+update test
+set A = 'A'
+where A = 'z';
